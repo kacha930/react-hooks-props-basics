ebfe1df4a6205f4786911e9ce7813b9858d50906
kacha930 <robiwere20@gmail.com>
2024-08-10 01:36:23 +0300

    understod

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 function BlogContent(props) {
+
+
+  // If we add a console.log in the BlogContent component to inspect the props: => // => { articleText: "Dear Reader: Bjarne Stroustrup has the perfect lecture oration." }
+
+  //This line is telling React to place the value that props.articleText represents within the <div>.
   return <div id="blog-content">{props.articleText}</div>;
 }
 
--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,10 +1,17 @@
 import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
+// We can add as many additional props as we want by assigning them in the parent component: <BlogContent
+                                                                            //articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration."
+                                                                             //isPublished={true}
+                                                                             //minutesToRead={1}
+                                                                                ///>
 
+   //Note: for props that are strings, we don't need to place curly braces around the values; for other data types (numbers, booleans, objects, etc), we need curly braces.                                                                             
 function BlogPost() {
   return (
     <div id="blog-post">
+    
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
